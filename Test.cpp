bf3f238918ff2272007355a65fdebae100d630d2
Jan <jan.gonzalez@enti.cat>
2021-02-25 10:35:16 +0100

    This is a test

---
+++ Test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+//Això és una prova
+
+int Sum(int a, b)
+{
+    int result = a + b;
+
+    return result;
+}
+
+int main()
+{
+    int a{0}, b{1};
+    int result = Sum(a, b);
+
+    std::cout << result << endl;
+}
