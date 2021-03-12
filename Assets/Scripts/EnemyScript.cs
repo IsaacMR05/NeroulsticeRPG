a3dfc9618a22a40585c1830c8e620c4571a17118
Adrià <mangaadri@gmail.com>
2021-03-12 09:49:42 +0100

    Started the movement, the collisions and enemy respawn

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public float velocity = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(Random.Range(-10,10)*velocity,Random.Range(-10,10)*velocity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MasterScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Respawn d'enemics aleatori
+
+
+public class MasterScript : MonoBehaviour
+{
+    public int numberOfEnemies = 0;
+    public GameObject enemies = null;
+    public int halfWidth = 0;
+    public int halfHeight = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(enemies != null)
+        {
+            for(int i =0; i < numberOfEnemies; i++)
+            {
+                Vector2 enemiesPosition = new Vector2(Random.Range(-halfWidth, halfWidth), Random.Range(-halfHeight, halfHeight));
+
+                GameObject.Instantiate(enemies, enemiesPosition, new Quaternion());
+            
+            }
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
