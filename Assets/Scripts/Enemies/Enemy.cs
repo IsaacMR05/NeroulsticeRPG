4ad70beafd5d5676f58455ec6c6ec29cd57ffa7d
Isaac <isaac.medina@enti.cat>
2021-03-19 09:31:34 +0100

    Created the enemies
    Their scripts, implemented their assets and collisions

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Attribute attributes; 
+    public  new string name;
+    public   int experience;
+
+
+   
+}
