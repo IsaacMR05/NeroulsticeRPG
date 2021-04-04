291f12d0dc340f78d2cc34f45330453132930b13
Isaac <isaac.medina@enti.cat>
2021-04-04 11:39:33 +0200

    AI
    Created and implemented the AI for the Slime and the Muatated Robot (The robot has to be fixed).

---
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    private Wandering wandering;
+    private EnemyAI ai;
+
+    public float attackDistance;
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wandering = GetComponent<Wandering>();
+        ai = GetComponent<EnemyAI>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        wandering.enabled = true;
+        ai.enabled = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, player.position) < attackDistance) 
+        {
+            wandering.enabled = false;
+            ai.enabled = true;
+        }
+        else
+        {
+            wandering.enabled = true;
+            ai.enabled = false;
+        }
+    }
+}
