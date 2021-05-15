4ed314da9abdabdae92fb3c87dbad0c7956acf97
Isaac <isaac.medina@enti.cat>
2021-05-15 19:57:47 +0200

    Main Menu & In-game Menu
    Created the Main Menu and working on the in-game menu

---
+++ Assets/Scripts/Menus/Main Menu/MainMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string gameScene;
+    public string demo;
+    public string twitter;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OpenURL()
+    {
+        Application.OpenURL(twitter);
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(gameScene);
+    }
+
+    public void Demo()
+    {
+        SceneManager.LoadScene(demo);
+
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
