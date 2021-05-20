99c8310b3aa3cb57726554ce7afeec1b8d948cd5
Isaac <isaac.medina@enti.cat>
2021-05-20 14:04:17 +0200

    Created a minimap on the screen
    Now the scenes have two cameras, one for the user to play and other as a minimap

---
+++ Assets/MiniMap.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMap : MonoBehaviour
+{
+
+    public Transform player;
+
+    void LateUpdate()
+    {
+        Vector3 newPosition;
+        newPosition.x = player.position.x;
+        newPosition.y = player.position.y;
+        newPosition.z = transform.position.z;
+
+        transform.position = newPosition;
+    }
+}
